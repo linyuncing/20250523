3accb3c5f8c3aa4968495d8fbe3d95dd7f479469
linyuncing <linyuncing2005@gmail.com>
2025-05-23 11:26:33 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let predictions = [];
 const indices = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
 const indices2 = [76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184];
 const leftEyeIndices = [243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112];
+const rightEyeIndices = [359, 467, 260, 259, 257, 258, 286, 414, 463, 341, 256, 252, 253, 254, 339, 255];
 
 function setup() {
   createCanvas(640, 480).position(
@@ -79,6 +80,18 @@ function draw() {
     beginShape();
     for (let i = 0; i < leftEyeIndices.length; i++) {
       const idx = leftEyeIndices[i];
+      const [x, y] = keypoints[idx];
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+
+    // 繪製右眼的紅色框線
+    stroke(255, 0, 0); // 紅色線條
+    strokeWeight(2);
+    noFill();
+    beginShape();
+    for (let i = 0; i < rightEyeIndices.length; i++) {
+      const idx = rightEyeIndices[i];
       const [x, y] = keypoints[idx];
       vertex(x, y);
     }
